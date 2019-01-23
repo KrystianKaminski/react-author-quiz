7a7fe00b16b93fe4663083046f6ba4e5b814b430
KrystianKaminski <krystiankaminski07@gmail.com>
2019-01-23 18:37:49 +0100

    Bootstrap installed

---
+++ src/AuthorQuiz.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+
+class AuthorQuiz extends Component {
+  render() {
+    return (
+      <div>Hello</div>
+    )
+  }
+}
+
+export default AuthorQuiz;
