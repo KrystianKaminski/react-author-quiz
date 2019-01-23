b5ae18b4d98bdce74b7c3602705873f01e852ce8
KrystianKaminski <krystiankaminski07@gmail.com>
2019-01-23 21:24:03 +0100

    Test for backgroundColor if no answer selected

--- src/AuthorQuiz.test.js
+++ src/AuthorQuiz.test.js
@@ -24,4 +24,16 @@ describe("Author Quiz", () => {
         const div = document.createElement('div')
         ReactDOM.render(<AuthorQuiz {...state} onAnswerSelected={() => { }} />, div)
     })
+
+    describe("When no answer has been selected", () => {
+        let wrapper;
+        beforeAll(() => {
+            wrapper = mount(<AuthorQuiz {...state} onAnswerSelected={() => { }} />)
+        })
+
+        it("should have no background color", () => {
+            expect(wrapper.find('div.row.turn').props().style.backgroundColor).toBe('')
+        })
+    })
+
 })
