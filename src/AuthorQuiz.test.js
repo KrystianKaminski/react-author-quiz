a3b1ee24d2cc6dd6a24c19e00649f2293aff5e3f
KrystianKaminski <krystiankaminski07@gmail.com>
2019-01-23 21:33:34 +0100

    Test for selecting answers

--- src/AuthorQuiz.test.js
+++ src/AuthorQuiz.test.js
@@ -60,11 +60,27 @@ describe("Author Quiz", () => {
             )
         })
 
-        it("should have a red background color", () => {
+        it("should have a green background color", () => {
             expect(wrapper.find('div.row.turn').props().style.backgroundColor).toBe('green')
         })
 
     })
 
+    describe('When the first answer is selected', () => {
+        let wrapper;
+        const handleAnswerSelected = jest.fn()
+        beforeAll(() => {
+            wrapper = mount(<AuthorQuiz {...state} onAnswerSelected={handleAnswerSelected} />)
+            wrapper.find('.answer').first().simulate('click')
+        })
+
+        it('onAnswerSelected should be called', () => {
+            expect(handleAnswerSelected).toHaveBeenCalled()
+        })
+
+        it('Selected receive The Shining', () => {
+            expect(handleAnswerSelected).toHaveBeenCalledWith('The Shining')
+        })
+    })
 
 })
