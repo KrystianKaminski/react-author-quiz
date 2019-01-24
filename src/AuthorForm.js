cffc6d09db42477c550fa8fa062e1f9c35a9f7f1
KrystianKaminski <krystiankaminski07@gmail.com>
2019-01-24 12:49:39 +0100

    Defined a state in AuthorForm component

--- src/AuthorForm.js
+++ src/AuthorForm.js
@@ -1,16 +1,32 @@
 import React from 'react'
 
 class AuthorForm extends React.Component {
+
+    constructor(props) {
+        super(props)
+        this.state = {
+            name: '',
+            imageUrl: ''
+        }
+    }
     render() {
         return (
             <form>
                 <div className="AddAuthorForm__input">
                     <label htmlFor="name">Name</label>
-                    <input type="text" name="name" />
+                    <input
+                        type="text"
+                        name="name"
+                        value={this.state.name}
+                    />
                 </div>
                 <div className="AddAuthorForm__input">
                     <label htmlFor="imageUrl">Image URL</label>
-                    <input type="text" name="imageUrl" />
+                    <input
+                        type="text"
+                        name="imageUrl"
+                        value={this.state.imageUrl}
+                    />
                 </div>
             </form>
         )
